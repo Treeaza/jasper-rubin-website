29ff0e1c82eca15ba4a686b3fc3e28e9eafa6c1c
Treeaza <jasper.rubin@gmail.com>
2021-05-14 20:47:39 -0230

    Clear out old project stuff on landing page

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,8 +1,8 @@
 class Slideshow {
-	constructor(length, images, dots) {
+	constructor(length, images, numberIndicator) {
 		this.length = length;
 		this.images = images;
-		this.dots = dots;
+		this.numberIndicator = numberIndicator;
 		this.position = 0;
 
 		this.adjust = function (increment) {
@@ -21,6 +21,8 @@ class Slideshow {
 			for (let i = 0; i < this.length; i++) {
 				this.images[i].style.display = i == this.position ? "initial" : "none";
 			}
+
+			this.numberIndicator.innerHTML = (this.position + 1) + "/" + this.length;
 		};
 	}
 }
@@ -33,19 +35,13 @@ function changeSlideshow(index, increment) {
 	slideshows[index].adjust(increment);
 }
 
-function Dot() {
-	let dot = document.createElement("span");
-	dot.classList.add("dot");
-	return dot;
-}
-
 var index = 0;
 SLIDESHOW_PARENTS.forEach(
 	element => {
 		// Get all images in slideshow and push into slideshow array
 		let images = element.querySelectorAll("img[class='slideshow-content']");
-		let dots = element.querySelector("div[class='slideshow-dots']");
-		slideshows.push(new Slideshow(images.length, images, dots));
+		let ind = element.querySelector("span[class='slideshow-number']");
+		slideshows.push(new Slideshow(images.length, images, ind));
 
 		// Add event listeners to button controls
 		element.querySelector("button[class='slideshow-left']").addEventListener("click", function (event) {
@@ -54,11 +50,6 @@ SLIDESHOW_PARENTS.forEach(
 		element.querySelector("button[class='slideshow-right']").addEventListener("click", function (event) {
 			changeSlideshow(index, 1);
 		});
-
-		// Set up index dots
-		for (let i = 0; i < images.length; i++) {
-			dots.appendChild(Dot());
-		}
 
 		// Load first image
 		changeSlideshow(index, 0);
